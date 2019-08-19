a29839952618462ec6276beb8abba429d9920a75
oriko <orikeidar01@gmail.com>
2019-08-19 13:36:48 +0300

    Create hardware objects

--- robot.py
+++ robot.py
@@ -1,24 +1,36 @@
+import os
+
 import magicbot
 import wpilib
-from components.chassis import Chassis
-from wpilib import Talon
 from ctre import WPI_TalonSRX
+from navx import AHRS
+from wpilib import SPI
+
+from components.chassis import Chassis
+from jokerpylib.mapping.map import Map
 
 """
 This is a template for magicbot project for FRC Team 4320 - The Jokerbot
 """
 
-class Jokerbot(magicbot.MagicRobot):
 
+class Jokerbot(magicbot.MagicRobot):
     chassis: Chassis
 
     def createObjects(self):
         '''Create motors and stuff here'''
-        self.chassis_left_master = WPI_TalonSRX()
-        self.chassis_left_slave = WPI_TalonSRX()
-        self.chassis_right_master = WPI_TalonSRX()
-        self.chassis_right_slave = WPI_TalonSRX()
 
+        with self.consumeExceptions():
+            Map.load_json(os.path.abspath("map.json"))
+
+        self.chassis_ports = Map.get_map()["subsystems"]["chassis"]["can_motors"]
+
+        self.chassis_left_master = WPI_TalonSRX(self.chassis_ports["left_master"])
+        self.chassis_left_slave = WPI_TalonSRX(self.chassis_ports["left_slave"])
+        self.chassis_right_master = WPI_TalonSRX(self.chassis_ports["right_master"])
+        self.chassis_right_slave = WPI_TalonSRX(self.chassis_ports["right_slave"])
+
+        self.chassis_navx = AHRS(SPI.Port.kMXP)
 
     def teleopInit(self):
         '''Called when teleop starts; optional'''
@@ -27,6 +39,7 @@ class Jokerbot(magicbot.MagicRobot):
     def teleopPeriodic(self):
         '''Called on each iteration of the control loop'''
         pass
+
 
 if __name__ == "__main__":
     wpilib.run(Jokerbot)
